a5c24b722149810f1a8a6e384b675c591cef3498
dan nguyen <dan.nguyen@eztable.com>
2020-01-07 18:00:24 +0700

    update table data

--- src/components/ViewData.js
+++ src/components/ViewData.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import BarChart from '../libs/chart-race-react';
 import randomcolor from 'randomcolor';
+import { Row, Col, Table } from 'antd';
+
+
 
 class ViewData extends Component {
 
@@ -22,7 +25,7 @@ class ViewData extends Component {
                     alignItems:'center',
                     justifyContent: 'center'
                     }}>
-                    {logo && <img src={logo} alt={name} style={{ width: '20px',                     marginRight: '8px'
+                    {logo && <img src={logo} alt={name} style={{ width: '20px',   height: '20px',marginRight: '8px'
 }}/>}
                     <div>{name}</div>
                 </div>
@@ -35,14 +38,32 @@ class ViewData extends Component {
 
     }
 
+    getColumnTable = (data) =>{
+        return Object.keys(data||{}).map(value=>{
+            if(value==='logo')
+                return ({
+                    title: value.toUpperCase(),
+                    dataIndex: value,
+                    render: (data) => <img src={data} alt='logo' style={{ width: '20px',  height: '20px'
+                }}/>,
+                });
+            return ({
+                title: value.toUpperCase(),
+                dataIndex: value,
+                fixed:value==='name'?true:false,
+            })
+        })
+    }
+
     render(){
         const { data,time,labels,colors,len } = this.getData(this.props.data);
         const barChartHeight = window.innerHeight - 188;
         const barMarginTop = 10;
         const dataLength = this.props.data.length;
         const barHeight = (barChartHeight - barMarginTop*dataLength)/dataLength;
-
-        return ( 
+        const columns=this.getColumnTable(this.props.data[0]);
+        return ( <Row gutter={[16, 16]}>
+            <Col span={12}>
             <BarChart 
                 start={true}
                 data={data} 
@@ -71,8 +92,16 @@ class ViewData extends Component {
                     }}
                 width={[15, 75, 10]}
                 maxItems={100}
+          
             />
-          )
+            </Col>
+            <Col span={12} style ={{
+                background:'#fff',
+               
+            }}>
+                <Table bordered={false} scroll={{ x: 1300 }} columns={columns} dataSource={this.props.data} pagination={false} size="small" />
+            </Col>
+        </Row> )
     }
 }
 export default ViewData;
